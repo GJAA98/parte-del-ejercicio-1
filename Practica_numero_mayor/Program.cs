882b2618404989acf9abe8c5c0cfcb8462b4c65a
GJAA98 <79035604+GJAA98@users.noreply.github.com>
2021-03-01 16:51:25 -0400

    Add files via upload
    Un programa que pida al usuario 10 números y luego calcule y muestre cuál es el mayor de todos ellos.

---
+++ Practica_numero_mayor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Practica_numero_mayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numeros = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Inserte un numero:");
+                string valor = Console.ReadLine();
+                numeros[i] = int.Parse(valor);
+            }
+
+            int numeroMayor = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (numeros[i] > numeroMayor)
+                {
+                    numeroMayor = numeros[i];
+                }
+            }
+
+            Console.WriteLine("Numero mayor:" + numeroMayor);
+
+
+        }
+    }
+}
